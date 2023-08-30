ff5c7cb9f12a4b4bd2866148a2bf668f731ddbc8
memefather <142188887+memefather@users.noreply.github.com>
2023-08-29 19:28:40 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,18 @@ from newsapi import NewsApiClient
 from datetime import date, timedelta
 from streamlit_extras.buy_me_a_coffee import button
 
-hide = """
+st.markdown(
+    """
     <style>
-.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; }
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
     </style>
-"""
-st.markdown(hide, unsafe_allow_html=True)
-
+    """,
+    unsafe_allow_html=True
+)
 # Init
 newsapi = NewsApiClient(api_key=os.getenv("NEWS_API_KEY"))
 
