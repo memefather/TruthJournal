0f5f0019a20e1b1b8df6b3487b4ebad5647e6c30
memefather <142188887+memefather@users.noreply.github.com>
2023-08-29 19:33:10 -0700

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,7 @@ from streamlit_extras.buy_me_a_coffee import button
 st.markdown(
     """
     <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
+.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } 
     </style>
     """,
     unsafe_allow_html=True
